009e92a7fc2f7e38e6eec11a4e76316015a9d5dd
Kamakarthik <karthik.k00012@gmail.com>
2025-11-23 02:17:27 +0530

    added CategoryFilter options

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,4 +1,25 @@
-function CategoryFilter() {
-    return <aside>category filter</aside>
+function CategoryFilter({ factsData, categoryColors }) {
+  return (
+    <aside>
+      <ul>
+        <li className="category">
+          <button className="btn btn-all-categories">All</button>
+        </li>
+        {factsData.map((fact) => {
+          const bgcolor = categoryColors[fact.category?.trim()] || '6b7280';
+          return (
+            <li className="category">
+              <button
+                className="btn btn-category"
+                style={{ backgroundColor: `#${bgcolor}` }}
+              >
+                {fact.category}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 }
 export default CategoryFilter;
