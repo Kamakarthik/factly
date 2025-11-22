050330cea1bb5d88cc17efc8ddfce395bd0ca912
Kamakarthik <karthik.k00012@gmail.com>
2025-11-22 23:24:16 +0530

    add FactVoteButtons component with voting options

---
+++ src/components/FactVoteButtons.jsx
@@ -0,0 +1,11 @@
+const FactVoteButtons = () => {
+  return (
+    <div className="vote-buttons">
+      <button>👍 24</button>
+      <button>🤯 9</button>
+      <button>⛔️ 4</button>
+    </div>
+  );
+};
+
+export default FactVoteButtons;
