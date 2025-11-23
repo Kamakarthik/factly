2c76fadfb011a0b7a13f35cadee3f36451532e61
Kamakarthik <karthik.k00012@gmail.com>
2025-11-23 19:08:41 +0530

    add FactForm Options

--- src/components/FactForm.jsx
+++ src/components/FactForm.jsx
@@ -1,5 +1,23 @@
-function FactForm() {
-  return <form className="fact-form">FactForm</form>;
+function FactForm({ categoryColors }) {
+  const categories = Object.keys(categoryColors);
+  return (
+    <form className="fact-form">
+      <input type="text" placeholder="Share a fact with the world..." />
+      <span>200</span>
+      <input type="text" placeholder="Trustworthy source..." />
+      <select>
+        <option value="">Choose category:</option>
+        {categories.map((category) => {
+          return (
+            <option value={category} key={category}>
+              {category.toUpperCase()}
+            </option>
+          );
+        })}
+      </select>
+      <button className="btn btn-large">Post</button>
+    </form>
+  );
 }
 
 export default FactForm;
