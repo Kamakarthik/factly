04d7d237323621a7c6cc4399a3213eb6e3259572
Kamakarthik <karthik.k00012@gmail.com>
2025-12-02 22:22:02 +0530

    updated script

---
+++ src/scripts/createAdmin.js
@@ -0,0 +1,27 @@
+// import { createClient } from '@supabase/supabase-js';
+
+// const supabaseKey = "SUPABASE_KEY_PLACEHOLDER";
+// const supabaseUrl = "SUPABASE_URL_PLACEHOLDER";
+// const supabase = createClient(supabaseUrl, supabaseKey);
+
+// async function createAdmin() {
+//   const { data, error } = await supabase.auth.signUp({
+//     email: 'example@email.com',
+//     password: 'securePassword123',
+//     options: {
+//       data: {
+//         username: 'adminUser',
+//         role: 'admin',
+//       },
+//     },
+//   });
+
+//   if (error) {
+//     console.error('Error creating admin:', error.message);
+//   } else {
+//     console.log('Admin created:', data.user);
+//   }
+// }
+
+// createAdmin();
+// export default createAdmin;
