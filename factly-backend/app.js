c5bb420f9ece9a6bfd194c62f4d586213001b560
Kamakarthik <karthik.k00012@gmail.com>
2025-12-08 20:25:17 +0530

    added  allowedOrigins

--- factly-backend/app.js
+++ factly-backend/app.js
@@ -57,9 +57,14 @@ app.use(
 );
 
 // CORS configuration
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:4173',
+  process.env.CLIENT_URL,
+];
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: allowedOrigins,
     credentials: true,
   })
 );
