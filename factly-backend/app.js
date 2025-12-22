b8f8b52ebc9a27681900d24cdb7680bab8e74edf
Kamakarthik <karthik.k00012@gmail.com>
2025-12-22 17:28:35 +0530

    Revert "fixed cookie parser"
    This reverts commit 7e6bc9ff1b1febe2f4f3af5f486ce2e17c42730a.

--- factly-backend/app.js
+++ factly-backend/app.js
@@ -4,7 +4,6 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
-const cookieParser = require('cookie-parser');
 const rateLimit = require('express-rate-limit');
 const morgan = require('morgan');
 
@@ -40,7 +39,6 @@ if (process.env.NODE_ENV === 'production') {
 
 // Body parser
 app.use(express.json({ limit: '10kb' }));
-app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
